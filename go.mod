96e7d25e1eb35d7c1adeecb755cc6be63311d75f
hOlrrDtr <danylo.shporta@nure.ua>
2023-11-24 17:59:00 +0200

    init

---
+++ go.mod
@@ -0,0 +1,17 @@
+module send_blob
+
+go 1.21
+
+require (
+	github.com/nullstyle/go-xdr v0.0.0-20180726165426-f4c839f75077 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/cast v1.5.1 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	gitlab.com/distributed_lab/figure v2.1.2+incompatible // indirect
+	gitlab.com/distributed_lab/logan v3.8.1+incompatible // indirect
+	gitlab.com/tokend/go v3.16.0+incompatible // indirect
+	gitlab.com/tokend/keypair v0.0.0-20190412110653-b9d7e0c8b312 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	depkeypair "gitlab.com/tokend/go/keypair"
+	"gitlab.com/tokend/go/signcontrol"
+	"io"
+	"net/http"
+	"os"
+)
+
+const URL = "http://localhost:8010/blob/"
+
+func CreateBlobRequest(data map[string]string, seedStr string) (*http.Request, error) {
+	str := `{"data": {"id": 3,"data": {`
+	num := 0
+	for key := range data {
+		num = 1
+		str += `"` + key + `": "` + data[key] + `",`
+	}
+	if num != 0 {
+		str = str[:len(str)-1]
+	}
+	str += `},"relationship": {"owner_id": ""}}}`
+	var jsonStr = []byte(str)
+	r, err := http.NewRequest("POST", URL, bytes.NewBuffer(jsonStr))
+	if err != nil {
+		return r, err
+	}
+	seed := depkeypair.MustParse(seedStr)
+	err = signcontrol.SignRequest(r, seed)
+	if err != nil {
+		return r, err
+	}
+	return r, nil
+}
+
+func BlobListRequest(seedStr string) (*http.Request, error) {
+	r, err := http.NewRequest("GET", URL, nil)
+	if err != nil {
+		return r, err
+	}
+	seed := depkeypair.MustParse(seedStr)
+	err = signcontrol.SignRequest(r, seed)
+	if err != nil {
+		return r, err
+	}
+	return r, nil
+}
+
+func GetBlobByTdRequest(seedStr string, blobId string) (*http.Request, error) {
+	r, err := http.NewRequest("GET", URL+blobId+"/", nil)
+	if err != nil {
+		return r, err
+	}
+	seed := depkeypair.MustParse(seedStr)
+	err = signcontrol.SignRequest(r, seed)
+	if err != nil {
+		return r, err
+	}
+	return r, nil
+}
+func DeleteBlobByIdRequest(seedStr string, blobId string) (*http.Request, error) {
+	url := "http://localhost:8010/blob/" + blobId + "/"
+	fmt.Println(url)
+	r, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return r, err
+	}
+	seed := depkeypair.MustParse(seedStr)
+	err = signcontrol.SignRequest(r, seed)
+	if err != nil {
+		return r, err
+	}
+	return r, nil
+}
+
+func Do(r *http.Request) {
+	client := &http.Client{}
+	resp, err := client.Do(r)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := io.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+
+}
+
+func main() {
+	e := false
+	for !e {
+		fmt.Println("Enter command letter:\ne-exit\nc-create\nd-delete\ng-get by id\nl - get list")
+		reader := bufio.NewReader(os.Stdin)
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("An error occured while reading input. Please try again", err)
+			return
+		}
+		switch command[0] {
+		case 'e':
+			e = true
+		case 'd':
+			fmt.Println("Enter blob id:")
+			blobId, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("An error occured while reading input. Please try again", err)
+				return
+			}
+			blobId = blobId[:len(blobId)-2]
+			fmt.Println("Enter your seed:")
+			seedStr, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("An error occured while reading input. Please try again", err)
+				return
+			}
+			seedStr = seedStr[:len(seedStr)-2]
+			r, err := DeleteBlobByIdRequest(seedStr, blobId)
+			if err != nil {
+				fmt.Println(err)
+				panic(err)
+			}
+			Do(r)
+		case 'l':
+			fmt.Println("Enter your seed:")
+			seedStr, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("An error occured while reading input. Please try again", err)
+				return
+			}
+			seedStr = seedStr[:len(seedStr)-2]
+			r, err := BlobListRequest(seedStr)
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			Do(r)
+		case 'g':
+			fmt.Println("Enter blob id:")
+			blobId, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("An error occured while reading input. Please try again", err)
+				return
+			}
+			blobId = blobId[:len(blobId)-2]
+			fmt.Println("Enter your seed:")
+			seedStr, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("An error occured while reading input. Please try again", err)
+				return
+			}
+			seedStr = seedStr[:len(seedStr)-2]
+			r, err := GetBlobByTdRequest(seedStr, blobId)
+			if err != nil {
+				fmt.Println(err)
+				panic(err)
+			}
+			Do(r)
+		case 'c':
+			s := false
+			data := make(map[string]string)
+			for !s {
+				fmt.Println("Enter key, or s to stop adding data:")
+				key, err := reader.ReadString('\n')
+				if err != nil {
+					fmt.Println("An error occured while reading input. Please try again", err)
+					return
+				}
+				switch key[:len(key)-2] {
+				case "s":
+					s = true
+				default:
+					fmt.Println("Enter value:")
+					value, err := reader.ReadString('\n')
+					if err != nil {
+						fmt.Println("An error occured while reading input. Please try again", err)
+						return
+					}
+					data[key[:len(key)-2]] = value[:len(value)-2]
+				}
+			}
+			fmt.Println("Enter your seed:")
+			seedStr, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("An error occured while reading input. Please try again", err)
+				return
+			}
+			seedStr = seedStr[:len(seedStr)-2]
+			r, err := CreateBlobRequest(data, seedStr)
+			if err != nil {
+				fmt.Println(err)
+				panic(err)
+			}
+			Do(r)
+
+		}
+	}
+}
